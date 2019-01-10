e025039e154c9963b20eaede68677b9340003432
Sebastian Civarolo <scivarolo@gmail.com>
2019-01-10 10:15:23 -0600

    set up planet_list

--- planets.py
+++ planets.py
@@ -1,1 +1,16 @@
 planet_list = ["Mercury", "Mars"]
+
+planet_list.append("Jupiter")
+planet_list.append("Saturn")
+
+planet_list.extend(["Uranus", "Neptune"])
+
+planet_list.insert(1, "Earth")
+planet_list.insert(1, "Venus")
+
+planet_list.append("Pluto")
+
+rocky_planets = planet_list[0:4]
+
+# Delete pluto since its a dwarf planet :(
+# del planet_list[-1]
