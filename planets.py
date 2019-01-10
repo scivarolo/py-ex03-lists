02ce42ceee4b450b28f2202d6cf90dbe441ce918
Sebastian Civarolo <scivarolo@gmail.com>
2019-01-10 10:15:43 -0600

    set up spacecraft visits and print visits to each planet

--- planets.py
+++ planets.py
@@ -14,3 +14,25 @@ rocky_planets = planet_list[0:4]
 
 # Delete pluto since its a dwarf planet :(
 # del planet_list[-1]
+
+spacecraft_visits = [
+    ('Pioneer 10', 'Jupiter'),
+    ('Pioneer 11', 'Jupiter', 'Saturn'),
+    ('Voyager 1', 'Jupiter', 'Saturn'),
+    ('Voyager 2', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'),
+    ('Galileo', 'Jupiter'),
+    ('Ulysses', 'Jupiter'),
+    ('Cassini-Huygens', 'Jupiter', 'Saturn'),
+    ('New Horizons', 'Jupiter', 'Pluto'),
+    ('Juno', 'Jupiter'),
+    ('Jupiter Icy Moons Explorer', 'Jupiter'),
+    ('Europa Clipper', 'Jupiter')
+]
+
+print('Spacecraft Visits')
+for planet in planet_list:
+    print()
+    print(f"----------{planet}----------")
+    for visit in spacecraft_visits:
+        if planet in visit:
+            print(visit[0])
